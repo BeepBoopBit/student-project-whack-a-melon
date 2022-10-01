e399d5af20502eeeaa16a6e14291699d988bc4e6
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-10-01 20:28:44 +0800

    Added addPanel() method

--- SpawnerLibrary/ImageSpawnerController.java
+++ SpawnerLibrary/ImageSpawnerController.java
@@ -7,8 +7,8 @@ import javax.swing.JPanel;
 
 // Handles all the Spawing Of Images
 public class ImageSpawnerController {
-    private static ImageSpawnerController     _instance       = null;
-    private ArrayList<GridPanel>    _panels         = new ArrayList<>();
+    private static ImageSpawnerController _instance = null;
+    private ArrayList<ImageSpawner> _spawners = new ArrayList<>();
     
     // Constructors
     private ImageSpawnerController(){}
@@ -21,6 +21,22 @@ public class ImageSpawnerController {
     
     // Relevant Functions
     public void addPanel(JPanel newPanel){
-        _panels.add(new GridPanel(newPanel));
+        // Create the new GridPanel using the newPanel
+        GridPanel newGridPanel = new GridPanel(newPanel);
+        
+        // Create the Image Spawner
+        ImageSpawner newSpawner = new ImageSpawner();
+        
+        // Attach the new spawner to the GridPanel
+        newSpawner.attachGridPanel(newGridPanel);
+        
+        // Add it to the array
+        _spawners.add(new ImageSpawner());
+    }
+    
+    public void startAll(){
+        for(int i = 0; i < _spawners.size(); ++i){
+            _spawners.get(i).start();
+        }
     }
 }
